3e0a1584b46c97869ddec478358b664249803909
Igor Karatayev <igor.cwer@gmail.com>
2020-03-20 14:47:09 +0300

    Refactored core library.

---
+++ GitStoryCLI/Program.cs
@@ -0,0 +1,31 @@
+﻿using GitStory.Core;
+using LibGit2Sharp;
+using System;
+using System.IO;
+
+namespace GitStoryCLI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string dir = Directory.GetCurrentDirectory();
+
+			using (var repo = new Repository(dir))
+			{
+				if (args.Length == 1 && args[0] == "fix")
+				{
+					repo.Fix();
+				}
+				else if (args.Length == 1 && args[0] == "status")
+				{
+
+				}
+				else
+				{
+					repo.Store();
+				}
+			}
+		}
+	}
+}
