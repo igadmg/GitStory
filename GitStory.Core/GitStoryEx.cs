7a95ba1d1a8e132e56d8b356257e9af502514cb9
Igor Karatayev <igor.cwer@gmail.com>
2020-03-28 22:36:55 +0300

    update

--- GitStory.Core/GitStoryEx.cs
+++ GitStory.Core/GitStoryEx.cs
@@ -122,7 +122,7 @@ namespace GitStory.Core
 			var storyBranch = repo.GetStoryBranch(repo.Head, storyBranchNameFn, out var storyBranchName);
 			storyBranch = storyBranch ?? repo.CreateBranch(storyBranchName);
 
-			var headRef2 = repo.Refs.Where(r => r.CanonicalName == head.CanonicalName).FirstOrDefault();
+			var headRef2 = repo.Refs.Where(r => r.CanonicalName == repo.Head.CanonicalName).FirstOrDefault();
 			headRef = repo.Head.Reference;
 			repo.Refs.UpdateTarget("HEAD", storyBranch.Reference.CanonicalName);
 		}
