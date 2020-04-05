532e3bbda91f265a0e2122fe1769d0266176ce30
Igor Karatayev <igor.cwer@gmail.com>
2020-04-05 23:15:43 +0300

    update

--- GitStoryVSIX/GitStoryVSPackage.cs
+++ GitStoryVSIX/GitStoryVSPackage.cs
@@ -29,7 +29,7 @@ namespace GitStoryVSIX
 	/// </para>
 	/// </remarks>
 	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-	[ProvideAutoLoad(UIContextGuids.CodeWindow, PackageAutoLoadFlags.BackgroundLoad)]
+	[ProvideAutoLoad(UIContextGuids.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
 	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
 	[Guid(GitStoryVSPackage.PackageGuidString)]
 	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
