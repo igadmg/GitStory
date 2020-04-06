1786e747fadb09562ec00a645b28c9187087042d
Igor Karatayev <igor.cwer@gmail.com>
2020-04-06 17:38:00 +0300

    update

--- GitStory.Core/LibGit2SharpEx.cs
+++ GitStory.Core/LibGit2SharpEx.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using LibGit2Sharp;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GitStory.Core
 {
-	class LibGit2SharpEx
+	public static class LibGit2SharpEx
 	{
+		static IEnumerable<Submodule> ForEachSubmodule(this IEnumerable<Submodule> submodules, Action<Submodule> fn)
+		{
+			var exceptions =
+				submodules.Select(sm => {
+					try { fn(sm); }
+					catch (Exception e) { return e; }
+					return null;
+				})
+				.Where(e => e != null)
+				.ToArray();
+
+			if (exceptions.Length != 0)
+				throw new AggregateException(exceptions);
+
+			return submodules;
+		}
 	}
 }
