2235b7bf9a7b26658248d3cb53254ca14a7f8024
Igor Karatayev <igor.cwer@gmail.com>
2020-04-06 17:16:37 +0300

    update

--- GitStory.Core/SwitchToStoryBranch.cs
+++ GitStory.Core/SwitchToStoryBranch.cs
@@ -20,5 +20,19 @@ namespace GitStory.Core
 		{
 			ToHeadBranch(repo, headRef);
 		}
+
+		static void ToStoryBranch(Repository repo, StoryBranchNameDelegate storyBranchNameFn, out Reference headRef)
+		{
+			var storyBranch = repo.GetStoryBranch(repo.Head, storyBranchNameFn, out var storyBranchName);
+			storyBranch = storyBranch ?? repo.CreateBranch(storyBranchName);
+
+			headRef = (repo.Head.Reference as SymbolicReference).Target;
+			repo.Refs.UpdateTarget("HEAD", storyBranch.Reference.CanonicalName);
+		}
+
+		static void ToHeadBranch(Repository repo, Reference headRef)
+		{
+			repo.Refs.UpdateTarget("HEAD", headRef.CanonicalName);
+		}
 	}
 }
