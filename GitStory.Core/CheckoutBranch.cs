ba7e10fabd44bdb2808892c557946728484c42bc
Igor Karatayev <igor.cwer@gmail.com>
2020-04-06 17:57:04 +0300

    update

---
+++ GitStory.Core/CheckoutBranch.cs
@@ -0,0 +1,27 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitStory.Core
+{
+	public class CheckoutBranch : IDisposable
+	{
+		Branch oldBranch;
+
+		public CheckoutBranch(Repository repo, Branch branch)
+		{
+			if (repo.RetrieveStatus(new StatusOptions { IncludeIgnored = false }).Any())
+				throw new Exception("Repository must have no changes.");
+
+			oldBranch = repo.Head;
+			Commands.Checkout(repo, branch);
+		}
+
+		public void Dispose()
+		{
+			
+		}
+	}
+}
